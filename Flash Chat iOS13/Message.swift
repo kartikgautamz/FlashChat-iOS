313d3a0b2960d900c92a2e438a806b9e1b875482
Kartik Gautam <95581576+kartikgautamz@users.noreply.github.com>
2024-09-16 19:40:21 +0530

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kartik Gautam on 01/07/24.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
